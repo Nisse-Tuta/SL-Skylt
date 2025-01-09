175fbc0529ff2922e4f751ee35e484452375e539
Nisse-Tuta <christer@hostklint.se>
2025-01-09 15:44:36 +0100

    Nudå

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SL_Skylt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
